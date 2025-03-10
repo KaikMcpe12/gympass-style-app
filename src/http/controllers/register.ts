68feacee22d9eb7fb762be5704d4b5187c39f7e9
Kaik <119709479+KaikMcpe12@users.noreply.github.com>
2025-03-10 18:51:44 -0300

    :sparkles: feat: new added design patterns

---
+++ src/http/controllers/register.ts
@@ -0,0 +1,36 @@
+import { PrismaUsersRepository } from '@/repositories/prisma/prisma-users-repository'
+import { UserAlredyExistsError } from '@/use-cases/errors/user-alredy-exists-error'
+import { RegisterUseCase } from '@/use-cases/register'
+import { FastifyReply, FastifyRequest } from 'fastify'
+import { z } from 'zod'
+
+export async function register(request: FastifyRequest, reply: FastifyReply) {
+  const registerSchema = z.object({
+    name: z.string(),
+    email: z.string().email(),
+    password: z.string().min(6),
+  })
+
+  const { name, email, password } = registerSchema.parse(request.body)
+
+  try {
+    const usersRepository = new PrismaUsersRepository()
+    const registerUseCase = new RegisterUseCase(usersRepository)
+
+    await registerUseCase.execute({
+      name,
+      email,
+      password,
+    })
+  } catch (err) {
+    if(err instanceof UserAlredyExistsError) {
+      return reply.status(409).send( {
+        message: err.message
+      })
+    }
+
+    return reply.status(500).send()
+  }
+
+  return reply.status(201).send()
+}
