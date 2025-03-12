63f54deb437a65adac11a6b9fa5935da9506caf2
Kaik <119709479+KaikMcpe12@users.noreply.github.com>
2025-03-12 18:45:42 -0300

    ✅ test: added new use-case test

--- src/http/controllers/register.ts
+++ src/http/controllers/register.ts
@@ -23,13 +23,13 @@ export async function register(request: FastifyRequest, reply: FastifyReply) {
       password,
     })
   } catch (err) {
-    if(err instanceof UserAlredyExistsError) {
-      return reply.status(409).send( {
-        message: err.message
+    if (err instanceof UserAlredyExistsError) {
+      return reply.status(409).send({
+        message: err.message,
       })
     }
 
-    return reply.status(500).send()
+    throw err
   }
 
   return reply.status(201).send()
